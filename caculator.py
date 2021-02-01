971e028df531e26d65f18c4697a1323d95dc944a
pipidankaqiu <77875926+pipidankaqiu@users.noreply.github.com>
2021-02-01 15:36:24 +0800

    Add files via upload

---
+++ caculator.py
@@ -0,0 +1,14 @@
+class Caculator:
+    pass
+
+    def jia(self,a,b):
+        return a+b
+
+    def jian(self,a,b):
+        return a-b
+
+    def chen(self,a,b):
+        return a*b
+
+    def chu(self,a,b):
+        return a/b
